311616e95027c6ec98903231d56f07942f1314f6
James Bennett <jamespjbennett@hotmail.com>
2016-04-22 07:48:01 +0000

    messing about with audio settings to get the single chord audio better

--- platforms/ios/www/js/play_sounds.js
+++ platforms/ios/www/js/play_sounds.js
@@ -1,4 +1,15 @@
-// var wind = new Wad({
-// source: 'noise'
-// });
-// wind.play();
+var saw = new Wad({source : 'sawtooth'})
+saw.play({
+    volume  : 0.8,
+    wait    : 0,     // Time in seconds between calling play() and actually triggering the note.
+    loop    : false, // This overrides the value for loop on the constructor, if it was set. 
+    pitch   : 'C4',  // A4 is 440 hertz.
+    label   : 'A',   // A label that identifies this note.
+    env     : {hold : 0.1, release: 1},
+    panning : [1, -1, 10],
+    filter  : {frequency : 100},
+    delay   : {delayTime : .8}
+})
+// setTimeout(function(){
+// 	saw.stop();
+// }, 200)
