83ebdb7dc489ee465082693f21275d2573813bd4
James Bennett <jamespjbennett@hotmail.com>
2016-04-22 12:16:31 +0000

    metronome now functioning

--- platforms/ios/www/js/beat_generation.js
+++ platforms/ios/www/js/beat_generation.js
@@ -1,13 +1,41 @@
 soundManager.setup({
   url: '/path/to/swf-files/',
   onready: function() {
-    var mySound = soundManager.createSound({
+    mySound = soundManager.createSound({
       id: 'aSound',
       url: '../../audio/RS.WAV'
     });
-    mySound.play();
+
+    function loopSound(sound) {
+      sound.play({
+        onfinish: function() {
+          setTimeout(function(){
+            loopSound(mySound);
+          }, 200)
+        }
+      });
+    }
+
+    function startMetronome(){
+      if($(this).hasClass('stopped')){
+        loopSound(mySound)
+        $(this).removeClass('stopped');
+        $(this).find('p').text('STOP');
+      }else{
+        mySound.stop();
+        $(this).addClass('stopped');
+        $(this).find('p').text('GO');
+
+      }
+    }
+   
+    $('#metronome').on('click', startMetronome);
   },
+
+  // mySound.stop();
   ontimeout: function() {
     // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
   }
 });
+
+
