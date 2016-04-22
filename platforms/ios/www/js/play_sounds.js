282068d50a5f6b67e3196e1e8d23a71a644111a3
James Bennett <jamespjbennett@hotmail.com>
2016-04-22 07:52:42 +0000

    cicles now responding to different notes

--- platforms/ios/www/js/play_sounds.js
+++ platforms/ios/www/js/play_sounds.js
@@ -1,11 +1,14 @@
+// set up the oscillator sound
 var saw = new Wad({source : 'sawtooth'})
 
 function playChord(){
+	var chord = $(this).attr('id');
+
 	saw.play({
 	    volume  : 0.8,
 	    wait    : 0,     // Time in seconds between calling play() and actually triggering the note.
 	    loop    : false, // This overrides the value for loop on the constructor, if it was set. 
-	    pitch   : 'C4',  // A4 is 440 hertz.
+	    pitch   : chord,  // A4 is 440 hertz.
 	    label   : 'A',   // A label that identifies this note.
 	    env     : {hold : 0.1, release: 1},
 	    panning : [1, -1, 10],
